9600549dcb7caf7c986a07377fa4177fa8879d72
nietras <nietras@users.noreply.github.com>
2026-01-02 17:23:24 +0100

    improve test

---
+++ src/SimdSharp.Test/AssertExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using System.Runtime.CompilerServices;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SimdSharp.Test;
+
+public static class AssertEx
+{
+    [return: NotNullIfNotNull(nameof(actual))]
+    public static T? AreEqualReturn<T>(T? expected, T? actual, string? message = "",
+        [CallerArgumentExpression(nameof(expected))] string expectedExpression = "",
+        [CallerArgumentExpression(nameof(actual))] string actualExpression = "")
+    {
+        Assert.AreEqual(expected, actual, message, expectedExpression, actualExpression);
+        return actual;
+    }
+}
---
+++ src/SimdSharp.Test/SimdTest_EnumerateLines.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SimdSharp.Test;
+
+[TestClass]
+public sealed class SimdTest_EnumerateLines
+{
+    public static IEnumerable<string> EnumerateLinesTestData =
+        [
+            string.Empty,
+            "single line",
+            "line\nwith\nLF",
+            "line\rwith\rCR",
+            "line\r\nwith\r\nCRLF",
+            "mix\r\nof\nline\rendings",
+            "trailing\nnewline\n",
+            "\nstarts with newline",
+            "\"quoted line\" and more",
+            //GenerateRandomText(seed: 1234, totalLength: 2_048, maxLineLength: 64)
+        ];
+
+    [TestMethod]
+    [DynamicData(nameof(EnumerateLinesTestData))]
+    public void SimdEnumerateLinesMatchesMemoryExtensions(string text)
+    {
+        var expectedEnumerator = MemoryExtensions.EnumerateLines(text);
+        var actualEnumerator = Simd.EnumerateLines(text);
+
+        while (AssertEx.AreEqualReturn(expectedEnumerator.MoveNext(), actualEnumerator.MoveNext()))
+        {
+            var expected = expectedEnumerator.Current;
+            var actual = actualEnumerator.Current;
+            Assert.AreEqual(expected.Length, actual.Length);
+            Assert.IsTrue(Unsafe.AreSame(ref MemoryMarshal.GetReference(expected), ref MemoryMarshal.GetReference(actual)));
+        }
+    }
+
+    private static string GenerateRandomText(int seed, int totalLength, int maxLineLength)
+    {
+        var random = new Random(seed);
+        var sb = new StringBuilder(totalLength);
+        var newlineVariants = new[] { "\n", "\r", "\r\n" };
+        var count = 0;
+
+        while (count < totalLength)
+        {
+            var lineLength = random.Next(1, maxLineLength + 1);
+            if (count + lineLength > totalLength)
+            {
+                lineLength = totalLength - count;
+            }
+
+            if (lineLength > 0)
+            {
+                sb.Append('a', lineLength);
+                count += lineLength;
+            }
+
+            if (count >= totalLength)
+            {
+                break;
+            }
+
+            var newline = newlineVariants[random.Next(newlineVariants.Length)];
+            if (count + newline.Length > totalLength)
+            {
+                break;
+            }
+
+            sb.Append(newline);
+            count += newline.Length;
+        }
+
+        return sb.ToString();
+    }
+}
